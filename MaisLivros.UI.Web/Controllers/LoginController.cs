490dec93e90222240184a2e4353d02008fa1e1dd
danzbz <danzbz07@gmail.com>
2023-04-29 17:55:31 -0300

    [ADD] - Tela de Cadastro de Usuário

---
+++ MaisLivros.UI.Web/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using MaisLivros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaisLivros.UI.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Entrar()
+        {
+            return View();
+        }
+    }
+}
