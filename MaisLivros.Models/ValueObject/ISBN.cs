91ab832f1de6438bd06eb350a11f458c3df34f66
danzbz <danzbz07@gmail.com>
2023-06-10 23:53:41 -0300

    Versionamento

---
+++ MaisLivros.Models/ValueObject/ISBN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaisLivros.Models.ValueObject
+{
+    public class ISBN
+    {
+        public string Value { get; }
+
+        public ISBN(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("O valor do ISBN não pode ser vazio ou nulo.");
+            }
+
+            Value = value;
+        }
+
+    }
+}
